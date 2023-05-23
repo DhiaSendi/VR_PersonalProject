57186bd3d8f6951eaf7d63ad9de9fe8d7b2ef0b6
Dhia Sendi <43573661+DhiaSendi@users.noreply.github.com>
2023-05-23 11:13:28 +0100

    Added File Browser plugin
    A free Unity asset: https://assetstore.unity.com/packages/p/runtime-file-browser-113006

---
+++ Assets/BookShelfManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookShelfManager : MonoBehaviour
+{
+    [SerializeField] GameObject shelf;
+    [SerializeField] int maxCovers;
+    [SerializeField] float offset;
+    void Start()
+    {
+        for(int i = 0; i < maxCovers;i++)
+        {
+            GameObject go = Instantiate(shelf, transform);
+            go.transform.localPosition = new Vector3(shelf.transform.localPosition.x - (offset * (i+1)), shelf.transform.localPosition.y, shelf.transform.localPosition.z);
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/RecordPlayerManager.cs
+++ Assets/RecordPlayerManager.cs
@@ -5,13 +5,44 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class RecordPlayerManager : XRSocketInteractor
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] bool isSelected, can;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
+    IXRSelectInteractable selectInteractable;
+    protected override void Awake()
     {
-        if(other.CompareTag("Disc"))
+        base.Awake();
+        audioSource = GetComponent<AudioSource>();
+    }
+    protected new void OnTriggerEnter(Collider other)
+     {
+        if (other.CompareTag("Disc"))
         {
+            audioClip = other.GetComponent<AudioSource>().clip;
 
-            other.GetComponent<AudioSource>().Play();
+            if (can)
+            {
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                print("Played once");
+            }
         }
+     }
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        isSelected = interactable.isSelected;
+        can = base.CanSelect(interactable);
+        selectInteractable = interactable;
+        return base.CanSelect(interactable);
+    }
+    private void Update()
+    {
+       // print("Is " + isSelected);
+       // print("Can " + can);
+       //print(selectInteractable);
+       //print(selectInteractable.isSelected);
+       //print(selectInteractable.transform.gameObject.name);
+
     }
 }
 
